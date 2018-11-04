9d6b7c8bb503befd955ad45907dfa59efb0a205d
Dola29 <danielosvaldoleal@hotmail.com>
2018-11-04 13:14:28 -0400

    URLs, Migrations and Eloquent

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/key', function(){
+    return str_random(32);
+    //return md5(uniqid());
+
+});
+
+$router->get('/users',['uses' =>'UsersController@index']);
